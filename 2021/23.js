65a60177a6a42cf70c82952b88b9ec9506752b93
tamas.toth <tamas.toth@emarsys.com>
2021-12-23 07:56:43 +0100

    AOC-2: Solve day 23 year 2021 by hand

---
+++ 2021/23.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function part1() {
+  return 'Solved by hand, solution is: 11120';
+}
+
+function part2() {
+  return 'Solved by hand, solution is: 49232';
+}
+
+console.log('part1: ' + part1());
+console.log('part2: ' + part2());
+
